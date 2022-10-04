8c7aef242a549599fa7f91d0b1d35e18780b90fb
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-10-04 11:24:41 +0200

    intall TAC withe CDN

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <div
+        class="youtube_player"
+        videoID="Gjrrgv4-S9k"
+        width="560"
+        height="315"
+        theme="light"
+        rel="0"
+        controls="1"
+        showinfo="1"
+        autoplay="0"
+      ></div>
     </div>
   );
 }
